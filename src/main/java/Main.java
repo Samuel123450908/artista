8d15eb4589599f62bfce5f08ca361b347ce10261
Samuel123450908 <169068157+Samuel123450908@users.noreply.github.com>
2024-05-13 14:58:06 +0000

    13 de maio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,61 @@ public class Main {
     System.out.println("ICharlie Brown JrI");
     System.out.println("I----------------I");
     System.out.println("");
+    System.out.println("-----------------------");
+    System.out.println("Album:Imunidade Musical");
+    System.out.println("-----------------------");
     System.out.println("");
+    System.out.println("------------------------------------------");
+    System.out.println("lançamento:esse album foi lançado em 2005");
+    System.out.println("------------------------------------------");
+    System.out.println("");
+    System.out.println("-------");
+    System.out.println("Musicas");
+    System.out.println("-------");
+    System.out.println("");
+    System.out.println("too fast live too young too die, essa musica tem duração de 40 segundos");
+     System.out.println("");
+     System.out.println("no passo a passo");
+     System.out.println("");
+     System.out.println("lutar pelo que e meu");
+     System.out.println("");
+     System.out.println("é quente");
+     System.out.println("");
+     System.out.println("onde não existe a paz não existe o amor");
+     System.out.println("");
+     System.out.println("ela vai voltar (todos os defeitos de uma mulher perfeita)");
+     System.out.println("");
+     System.out.println("o mundo explodiu la fora");
+    System.out.println("");
+    System.out.println("senhor do tempo");
+    System.out.println("");
+    System.out.println("liberdade acima de tudo");
+    System.out.println("");
+     System.out.println("pra não dizer que não falei das flores");
+     System.out.println("");
+     System.out.println("abrir seus olhos");
+     System.out.println("");
+     System.out.println("green goes");
+     System.out.println("");
+     System.out.println("i feel so good today");
+     System.out.println("peso da batida do errado que deu certo");
+     System.out.println("");
+     System.out.println("aquela paz");
+     System.out.println("");
+     System.out.println("onde esta o mundo bom? (living l.a.)");
+    
+    
+    
+    
+    
+
+
+
+
+    
+    
+
+    
     
 }
 }
