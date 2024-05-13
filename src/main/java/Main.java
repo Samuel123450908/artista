b0bfbbc76a84f9c8a09a9b2c1d70f5c392364cde
Samuel123450908 <169068157+Samuel123450908@users.noreply.github.com>
2024-05-13 14:37:49 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("I----------------I");
+    System.out.println("ICharlie Brown JrI");
+    System.out.println("I----------------I");
+    System.out.println("");
+    System.out.println("");
+    
+}
+}
